23796fa0461a94587f04e9017de26110f18c5883
Stanislaw Korczykowski <stanislaw.korczykowski@gmail.com>
2023-03-30 21:21:47 +0200

    Śmigający kodzik

--- main.py
+++ main.py
@@ -1,68 +1,82 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Parametry wahadła
+g = 9.81  # przyspieszenie ziemskie (m/s^2)
+l = 1.0  # długość wahadła (m)
+theta0 = np.pi / 4  # początkowy kąt wychylenia (rad)
+omega0 = 0.0  # początkowa prędkość kątowa (rad/s)
 
-g = 9.81
-l = 1.0
-theta0 = np.pi / 4
-omega0 = 0.0
+# Parametry symulacji
+t0 = 0.0  # czas startowy (s)
+t_end = 10.0  # czas końcowy (s)
+dt = 0.01  # krok czasowy (s)
+method = input().strip()
 
 
-t0 = 0.0
-t_end = 10.0
-dt = 0.01
+def euler(theta, omega, dt):
+    theta_new = theta + dt * omega
+    omega_new = omega - (dt * g / l) * np.sin(theta)
+    return theta_new, omega_new
 
-method = input()
 
-def wzor_wahadla(y):
-    theta, omega = y
-    dydt = np.array([omega, -(g / l) * np.sin(theta)])
-    return dydt
+def euler_improved(theta, omega, dt):
+    omega_new = omega - (dt * g / l) * np.sin(theta)
+    theta_new = theta + dt * omega_new
+    return theta_new, omega_new
 
-def rk4_step(f, y, dt):
-    k1 = f(y)
-    k2 = f(y + 0.5 * dt * k1)
-    k3 = f(y + 0.5 * dt * k2)
-    k4 = f(y + dt * k3)
-    return y + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+def rk4(theta, omega, dt):
+    k1_theta = dt * omega
+    k1_omega = dt * (-(g / l) * np.sin(theta))
+
+    k2_theta = dt * (omega + 0.5 * k1_omega)
+    k2_omega = dt * (-(g / l) * np.sin(theta + 0.5 * k1_theta))
+
+    k3_theta = dt * (omega + 0.5 * k2_omega)
+    k3_omega = dt * (-(g / l) * np.sin(theta + 0.5 * k2_theta))
+
+    k4_theta = dt * (omega + k3_omega)
+    k4_omega = dt * (-(g / l) * np.sin(theta + k3_theta))
+
+    theta_new = theta + (k1_theta + 2 * k2_theta + 2 * k3_theta + k4_theta) / 6
+    omega_new = omega + (k1_omega + 2 * k2_omega + 2 * k3_omega + k4_omega) / 6
+
+    return theta_new, omega_new
+
+
+# Główna pętla symulacji
 t = t0
-y = [theta0, omega0]
+theta, omega = theta0, omega0
 angle = [theta0]
 trajectory = []
 
-def euler(f, y, dt):
-    return y + dt * f(y)
-def euler_improved(f, y, dt):
-    dydt = f(y)
-    omega_new = y[1] + dt * dydt[1]
-    theta_new = y[0] + dt * omega_new
-    return np.array([theta_new, omega_new])
-
-
-print("wybierz metode: [euler, eulerimp, rk4]")
 while t <= t_end:
     if method == "euler":
-        y = euler(wzor_wahadla, y, dt)
-    if method == "rk4":
-        y = rk4_step(wzor_wahadla, y, dt)
-    if method == "eulerimp":
-        y = euler_improved(wzor_wahadla, y, dt)
-
-
+        theta, omega = euler(theta, omega, dt)
+    elif method == "euler_improved":
+        theta, omega = euler_improved(theta, omega, dt)
+    elif method == "rk4":
+        theta, omega = rk4(theta, omega, dt)
+    else:
+        print("Nieznana metoda: wybierz 'euler', 'eulerimp' lub 'rk4'")
+        method = input().strip()
 
     t += dt
-    angle.append(y[0])
-    trajectory.append((l * np.sin(y[0]), -l * np.cos(y[0])))
+    angle.append(theta)
+    trajectory.append((l * np.sin(theta), -l * np.cos(theta)))
 
 # Konwersja trajektorii na współrzędne kartezjańskie
-x = [x[0] for x in trajectory]
-y = [y[1] for y in trajectory]
+trajectory = np.array(trajectory)
+x = trajectory[:, 0]
+y = trajectory[:, 1]
 
 # Wizualizacja trajektorii wahadła
 plt.plot(x, y)
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("Trajektoria wahadła RK4")
+plt.xlabel("X [m]")
+plt.ylabel("Y [m]")
+plt.title(f"Trajektoria wahadła (metoda {method.upper()})")
 plt.axis("equal")
 plt.grid()
 plt.show()
+
